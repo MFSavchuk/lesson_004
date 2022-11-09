b72a1e3d4b39aab4632e2db23ce6281365cfe7ce
Mikhail Savchuk <mr.savchuk@bk.ru>
2022-11-09 10:16:18 +0300

    05 +

--- 05_snowfall.py
+++ 05_snowfall.py
@@ -18,25 +18,27 @@ sd.background_color = sd.COLOR_BLACK
 # sd.user_want_exit()
 
 N = 30  # количество снежинок
+x_cords = []  # координаты x
+y_cords = []  # координаты y
+sizes = []  # размеры снежинок
+snowflakes = []  # список снежинок
+factor_a = []
+factor_b = []
+factor_c = []
+count = 0  # счетчик для увеличения сугроба
 
 while True:
-    x_cords = []  # координаты x
-    y_cords = []  # координаты y
-    sizes = []  # размеры снежинок
-    snowflakes = []  # список снежинок
-    factor_a = []
-    factor_b = []
-    factor_c = []
-    all_cords_y = []
-
+    if count > 100:
+        print('Высота сугроба 100мм')
+        break
     for i in range(N):
-        random = sd.random_number(20, 580)
+        random = sd.random_number(0, 600)
         x_cords.append(random)
 
         random = sd.random_number(600, 1200)
         y_cords.append(random)
 
-        random = sd.random_number(5, 50)
+        random = sd.random_number(15, 50)
         sizes.append(random)
 
         random = float((sd.random_number(50, 70)) / 100)
@@ -50,8 +52,12 @@ while True:
 
     while True:
         sd.clear_screen()
-        for i in range(N):
-            if y_cords[i] < 15:
+        if max(y_cords) < 50+count:
+            count += 20
+            break
+
+        for i in range(len(y_cords)):
+            if y_cords[i] < sd.random_number(25+count, 49+count):
                 center_point = sd.get_point(x_cords[i], y_cords[i])
                 sd.snowflake(center=center_point, length=sizes[i], color=sd.COLOR_WHITE, factor_a=factor_a[i],
                              factor_b=factor_b[i], factor_c=factor_c[i])
@@ -61,10 +67,8 @@ while True:
             center_point = sd.get_point(x_cords[i] - wind, y_cords[i])
             sd.snowflake(center=center_point, length=sizes[i], color=sd.COLOR_WHITE, factor_a=factor_a[i],
                          factor_b=factor_b[i], factor_c=factor_c[i])
-            y_cords[i] -= sd.random_number(10, 10)
+            y_cords[i] -= sd.random_number(2, 10)
 
-        if max(y_cords) < 15:
-            break
         sd.sleep(0.1)
         if sd.user_want_exit():
             break
